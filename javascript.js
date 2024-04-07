0b3bb6da15f01307a403d8ecca2069fd0d15f2dd
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-06 23:22:32 -0400

    Add function multiply()

--- javascript.js
+++ javascript.js
@@ -5,3 +5,7 @@ function add(operand1, operand2) {
 function subtract(oprand1, operand2) {
   return operand1 - operand2;
 }
+
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
+}
