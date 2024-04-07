38f832c8bdba2ffb1d9543053eb4c40fc0e0e639
Raul Acosta <107330142+racodess@users.noreply.github.com>
2024-04-06 23:23:44 -0400

    Add function divide()

--- javascript.js
+++ javascript.js
@@ -9,3 +9,7 @@ function subtract(oprand1, operand2) {
 function multiply(operand1, operand2) {
   return operand1 * operand2;
 }
+
+function divide(operand1, operand2) {
+  return operand1 / operand2;
+}
